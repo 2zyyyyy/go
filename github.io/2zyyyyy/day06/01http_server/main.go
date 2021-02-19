1895319a353de2c5ff22f1be0c9200273b02df2f
2zyyyyy <2zyyyyy4dev@gmail.com>
2021-02-19 22:34:00 +0800

    net/http

--- github.io/2zyyyyy/day06/01http_server/main.go
+++ github.io/2zyyyyy/day06/01http_server/main.go
@@ -9,14 +9,43 @@ import (
 // net/http
 
 func f1(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadFile("../html.txt")
+	b, err := ioutil.ReadFile("./html.txt")
 	if err != nil {
 		w.Write([]byte(fmt.Sprintf("%v", err)))
 	}
-	w.Write([]byte(b))
+	w.Write(b)
+}
+
+func getRequest(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	data := r.URL.Query()
+	name := data.Get("name")
+	age := data.Get("age")
+	fmt.Println(name, age)
+	answer := `{"status": "GET OK"}`
+	w.Write([]byte(answer))
+}
+
+func postRequest(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	// 1. 请求类型是application/x-www-form-urlencoded时解析form数据
+	r.ParseForm()
+	fmt.Println("form:", r.PostForm) // 打印form表单数据
+	fmt.Println("name:"+r.PostForm.Get("name"), "age:"+r.PostForm.Get("age"))
+	// 2. 请求类型是application/json时从r.Body读取数据
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("read request.body failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("b:", string(b))
+	answer := `{"status":"POST OK"}`
+	w.Write([]byte(answer))
 }
 
 func main() {
-	http.HandleFunc("/golang/page/", f1)
+	http.HandleFunc("/golang/page", f1)
+	http.HandleFunc("/golang/page/get", getRequest)
+	http.HandleFunc("/golang/page/post", postRequest)
 	http.ListenAndServe("127.0.0.1:9090", nil)
 }
