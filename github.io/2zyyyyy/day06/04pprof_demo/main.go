49b4097cec4e7fa6d805ddb4ba6467d02c2014f0
月满轩尼诗 <android4google@163.com>
2021-05-11 21:34:56 +0800

    性能优化01

---
+++ github.io/2zyyyyy/day06/04pprof_demo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// 一段有问题的代码
+func logicCode() {
+	var c chan int
+	for {
+		select {
+		case v := <-c:
+			fmt.Printf("recv from chan, value:%v\n", v)
+		default:
+		}
+	}
+}
