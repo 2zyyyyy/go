6826717b0563e129b46b12e22915c82b2733ed45
月满轩尼诗 <android4google@163.com>
2021-05-22 10:42:35 +0800

    add:cpu pprof test

--- github.io/2zyyyyy/day06/04pprof_demo/main.go
+++ github.io/2zyyyyy/day06/04pprof_demo/main.go
@@ -21,26 +21,31 @@ func logicCode() {
 		case v := <-c: // 阻塞
 			fmt.Printf("recv from chan, value:%v\n", v)
 		default:
+			time.Sleep(time.Second)
 		}
 	}
 }
 
 func main() {
-	var isCPUPprof bool
-	var isMemPprof bool
+	var isCPUPprof bool // 是否开启CPUprofile的标志位
+	var isMemPprof bool // 是否开启内存profile的标志位
 
 	flag.BoolVar(&isCPUPprof, "cpu", false, "turn cpu pprof on")
 	flag.BoolVar(&isMemPprof, "mem", false, "turn mem pprof on")
 	flag.Parse()
 
 	if isCPUPprof {
-		file, err := os.Create("./cpu.pprof")
+		f1, err := os.Create("./cpu.pprof") // 在当前路径创建一个cpu profile文件
 		if err != nil {
 			fmt.Printf("create cpu pprof failed, err:%v\n", err)
 			return
 		}
-		pprof.StartCPUProfile(file)
-		defer pprof.StopCPUProfile()
+		pprof.StartCPUProfile(f1) // 往文件中写入CPU profile信息
+		defer func() {
+			pprof.StopCPUProfile()
+			f1.Close()
+		}()
+
 	}
 	for i := 0; i < 6; i++ {
 		go logicCode()
@@ -48,12 +53,12 @@ func main() {
 	time.Sleep(20 * time.Second)
 
 	if isMemPprof {
-		file, err := os.Create("./mem.pprof")
+		f2, err := os.Create("./mem.pprof")
 		if err != nil {
 			fmt.Printf("create mem pprof failed, err:%v\n", err)
 			return
 		}
-		pprof.WriteHeapProfile(file)
-		file.Close()
+		pprof.WriteHeapProfile(f2)
+		f2.Close()
 	}
 }
