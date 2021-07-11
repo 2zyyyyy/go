e725635cf5a88fc49e322265adc7393ef39ba8ff
2zyyyyy <android4google@163.com>
2021-07-11 20:26:01 +0800

    add:kafka测试

---
+++ github.io/2zyyyyy/day09/10kafka/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+// kafka demo
+
+// 基于sarama第三方库开发的Kafka client
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll          // 发送完数据需要leader和follow都确认
+	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
+	config.Producer.Return.Successes = true                   // 成功交付的消息将在success channel返回
+
+	// 构造一个消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "web_log"
+	msg.Value = sarama.StringEncoder("this is a test log;")
+
+	// 连接kafka
+	client, err := sarama.NewSyncProducer([]string{"127.0.0.1:9092"}, config)
+	if err != nil {
+		fmt.Println("producer closed, err:", err)
+		return
+	}
+	fmt.Println("kafka连接成功！")
+	defer func(client sarama.SyncProducer) {
+		err := client.Close()
+		if err != nil {
+			fmt.Println("client closed failed, err:", err)
+		}
+	}(client)
+
+	// 发送消息
+	pid, offset, err := client.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send msg failed, err:", err)
+		return
+	}
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
