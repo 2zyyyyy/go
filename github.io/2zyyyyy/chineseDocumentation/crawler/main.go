d85d537d285bfc9fe5eca6ce8b97531947cad019
wanli <zhangyun@ifugle.com>
2022-03-01 21:08:46 +0800

    add:新增爬虫案例

--- github.io/2zyyyyy/chineseDocumentation/crawler/main.go
+++ github.io/2zyyyyy/chineseDocumentation/crawler/main.go
@@ -11,22 +11,19 @@ import (
 	"time"
 )
 
-// 爬虫案例
-
-// 封装错误处理
 func HandleError(err error, why string) {
 	if err != nil {
 		fmt.Println(why, err)
 	}
 }
 
-// 下载图片 传入的是图片叫什么
+// 下载图片，传入的是图片叫什么
 func DownloadFile(url string, filename string) (ok bool) {
-	res, err := http.Get(url)
+	resp, err := http.Get(url)
 	HandleError(err, "http.get.url")
-	defer res.Body.Close()
-	bytes, err := ioutil.ReadAll(res.Body)
-	HandleError(err, "res.body")
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	HandleError(err, "resp.body")
 	filename = "/Users/gilbert/go/src/go/github.io/2zyyyyy/chineseDocumentation/crawler/image" + filename
 	// 写出数据
 	err = ioutil.WriteFile(filename, bytes, 0666)
@@ -37,39 +34,59 @@ func DownloadFile(url string, filename string) (ok bool) {
 	}
 }
 
-// 并发爬思路
-// 1.初始化数据通道
-// 2.爬虫写出：26个协程向通道中添加图片链接
-// 3.任务统计协程：检查26个任务是否都完成，完成就关闭通道
-// 4.下载协程：从通道里读取链接并下载数据
+// 并发爬思路：
+// 1.初始化数据管道
+// 2.爬虫写出：26个协程向管道中添加图片链接
+// 3.任务统计协程：检查26个任务是否都完成，完成则关闭数据管道
+// 4.下载协程：从管道里读取链接并下载
 
 var (
-	chanImageUrls chan string // 存放图片链接通道
-	wg            sync.WaitGroup
+	// 存放图片链接的数据管道
+	chanImageUrls chan string
+	waitGroup     sync.WaitGroup
 	// 用于监控协程
 	chanTask chan string
-	reImage  = `https?://[^"]+?(\.((jpg)|(png)|(jpeg)|(gif)|(bmp)))`
+	reImg    = `https?://[^"]+?(\.((jpg)|(png)|(jpeg)|(gif)|(bmp)))`
 )
 
 func main() {
-	// 1.通道初始化
+	// myTest()
+	// DownloadFile("http://i1.shaodiyejin.com/uploads/tu/201909/10242/e5794daf58_4.jpg", "1.jpg")
+
+	// 1.初始化管道
 	chanImageUrls = make(chan string, 1000000)
 	chanTask = make(chan string, 26)
 	// 2.爬虫协程
-	for i := 0; i < 27; i++ {
-		wg.Add(1)
-		go get
+	for i := 1; i < 27; i++ {
+		waitGroup.Add(1)
+		go getImgUrls("https://www.bizhizu.cn/shouji/tag-%E5%8F%AF%E7%88%B1/" + strconv.Itoa(i) + ".html")
 	}
+	// 3.任务统计协程，统计26个任务是否都完成，完成则关闭管道
+	waitGroup.Add(1)
+	go CheckOK()
+	// 4.下载协程：从管道中读取链接并下载
+	for i := 0; i < 5; i++ {
+		waitGroup.Add(1)
+		go DownloadImg()
+	}
+	waitGroup.Wait()
 }
 
 // 下载图片
-func DownloadImage() {
+func DownloadImg() {
 	for url := range chanImageUrls {
-		filename := G
+		filename := GetFilenameFromUrl(url)
+		ok := DownloadFile(url, filename)
+		if ok {
+			fmt.Printf("%s 下载成功\n", filename)
+		} else {
+			fmt.Printf("%s 下载失败\n", filename)
+		}
 	}
+	waitGroup.Done()
 }
 
-// 获取url名字
+// 截取url名字
 func GetFilenameFromUrl(url string) (filename string) {
 	// 返回最后一个/的位置
 	lastIndex := strings.LastIndex(url, "/")
@@ -89,15 +106,15 @@ func CheckOK() {
 		fmt.Printf("%s 完成了爬取任务\n", url)
 		count++
 		if count == 26 {
-			clsoe(chanImageUrls)
+			close(chanImageUrls)
 			break
 		}
 	}
-	wg.Done()
+	waitGroup.Done()
 }
 
-// 爬图片链接存放到通道
-// url传的是整页的链接
+// 爬图片链接到管道
+// url是传的整页链接
 func getImgUrls(url string) {
 	urls := getImgs(url)
 	// 遍历切片里所有链接，存入数据管道
@@ -105,18 +122,18 @@ func getImgUrls(url string) {
 		chanImageUrls <- url
 	}
 	// 标识当前协程完成
-    // 每完成一个任务，写一条数据
-    // 用于监控协程知道已经完成了几个任务
+	// 每完成一个任务，写一条数据
+	// 用于监控协程知道已经完成了几个任务
 	chanTask <- url
-	wg.Done()
+	waitGroup.Done()
 }
 
-// 获取当前页面图片链接
+// 获取当前页图片链接
 func getImgs(url string) (urls []string) {
 	pageStr := GetPageStr(url)
-	re := regexp.MustCompile(reImage)
+	re := regexp.MustCompile(reImg)
 	results := re.FindAllStringSubmatch(pageStr, -1)
-	fmt.Printf("共找到%d条结果/n", len(results))
+	fmt.Printf("共找到%d条结果\n", len(results))
 	for _, result := range results {
 		url := result[0]
 		urls = append(urls, url)
@@ -125,8 +142,14 @@ func getImgs(url string) (urls []string) {
 }
 
 // 抽取根据url获取内容
-func GetPageStr(url string) (page string)  {
-	res, err := http.Get(url)
-	HandError(err, "http.Get url")
-	defer res.
+func GetPageStr(url string) (pageStr string) {
+	resp, err := http.Get(url)
+	HandleError(err, "http.Get url")
+	defer resp.Body.Close()
+	// 2.读取页面内容
+	pageBytes, err := ioutil.ReadAll(resp.Body)
+	HandleError(err, "ioutil.ReadAll")
+	// 字节转字符串
+	pageStr = string(pageBytes)
+	return pageStr
 }
