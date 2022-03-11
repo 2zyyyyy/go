4573afc16c55238f48c3e42b1d7ea4a0cf15e1c0
wanli <zhangyun@ifugle.com>
2022-03-11 18:28:40 +0800

    add:kafka介绍

--- github.io/2zyyyyy/chineseDocumentation/zookeeper/client/main.go
+++ github.io/2zyyyyy/chineseDocumentation/zookeeper/client/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/samuel/go-zookeeper/zk"
 	"io/ioutil"
+	"math/rand"
 	"net"
 	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
 )
 
 func checkError(err error) {
@@ -13,35 +16,36 @@ func checkError(err error) {
 		fmt.Println(err)
 	}
 }
-
 func main() {
 	for i := 0; i < 100; i++ {
 		startClient()
+
 		time.Sleep(1 * time.Second)
 	}
 }
 
 func startClient() {
 	// service := "127.0.0.1:8899"
-	// 获取地址
+	//获取地址
 	serverHost, err := getServerHost()
 	if err != nil {
-		fmt.Printf("get server host fail: %s\n", err)
+		fmt.Printf("get server host fail: %s \n", err)
 		return
 	}
-	fmt.Println("connect host:" + serverHost)
+
+	fmt.Println("connect host: " + serverHost)
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", serverHost)
 	checkError(err)
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
 	defer conn.Close()
 
-	_,err = conn.Write([]byte("timestamp"))
+	_, err = conn.Write([]byte("timestamp"))
 	checkError(err)
 
-	res, err := ioutil.ReadAll(conn)
+	result, err := ioutil.ReadAll(conn)
 	checkError(err)
-	fmt.Println(string(res))
+	fmt.Println(string(result))
 
 	return
 }
@@ -49,24 +53,36 @@ func startClient() {
 func getServerHost() (host string, err error) {
 	conn, err := GetConnect()
 	if err != nil {
-		fmt.Printf("connect zk error:%s\n", err)
+		fmt.Printf(" connect zk error: %s \n ", err)
+		return
+	}
+	defer conn.Close()
+	serverList, err := GetServerList(conn)
+	if err != nil {
+		fmt.Printf(" get server list error: %s \n", err)
 		return
 	}
 
-	defer conn.Close()
-	serverList, err := GetServerList()
-}
+	count := len(serverList)
+	if count == 0 {
+		err = errors.New("server list is empty \n")
+		return
+	}
 
-func GetServerList(conn *zk.Conn) (list []string, err error) {
-	list, _, err = conn.Children("/go_servers")
+	//随机选中一个返回
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	host = serverList[r.Intn(3)]
 	return
 }
-
 func GetConnect() (conn *zk.Conn, err error) {
 	zkList := []string{"localhost:2181"}
 	conn, _, err = zk.Connect(zkList, 10*time.Second)
 	if err != nil {
 		fmt.Println(err)
 	}
+	return
+}
+func GetServerList(conn *zk.Conn) (list []string, err error) {
+	list, _, err = conn.Children("/go_servers")
 	return
 }
