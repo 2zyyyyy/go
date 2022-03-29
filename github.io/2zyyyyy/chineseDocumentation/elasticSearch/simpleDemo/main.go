15c9e28e5d353b90a69b158f82ceaab1af5e6de3
wanli <zhangyun@ifugle.com>
2022-03-29 19:54:44 +0800

    update:simpleDemo/main.go

--- github.io/2zyyyyy/chineseDocumentation/elasticSearch/simpleDemo/main.go
+++ github.io/2zyyyyy/chineseDocumentation/elasticSearch/simpleDemo/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-
 	"github.com/olivere/elastic/v7"
 )
 
@@ -17,20 +16,16 @@ type Person struct {
 
 func main() {
 	client, err := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
+	// handle error
 	if err != nil {
-		// Handle error
 		panic(err)
 	}
-
-	fmt.Println("connect to es success")
-	p1 := Person{Name: "lmh", Age: 18, Married: false}
-	put1, err := client.Index().
-		Index("user").
-		BodyJson(p1).
-		Do(context.Background())
+	fmt.Println("connect to es success!")
+	p1 := Person{Name: "2zyyyyy", Age: 25, Married: false}
+	put, err := client.Index().Index("user").BodyJson(p1).Do(context.Background())
+	// handle error
 	if err != nil {
-		// Handle error
 		panic(err)
 	}
-	fmt.Printf("Indexed user %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
+	fmt.Printf("Indexed user %s to index %s, type %s\n", put.Id, put.Index, put.Type)
 }
