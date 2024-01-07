95a5ea829a689742eecd9dd664d2ba72c69c30d5
gilbert <2zyyyyy4dev@gmail.com>
2024-01-07 18:57:40 +0800

    update：修改注释

--- github.io/2zyyyyy/main.go
+++ github.io/2zyyyyy/main.go
@@ -9,16 +9,16 @@ import "fmt"
 // }
 
 func main() {
-	// n := 199
-	// sum := 0
-	// for n > 0 {
-	// 	sum += n % 10
-	// 	fmt.Println("sum:", sum)
-	// 	n = n / 10
-	// 	fmt.Println("n:", n)
-	// }
-	// fmt.Println("各位数的和sum为:", sum)
-	// test()
+	n := 199
+	sum := 0
+	for n > 0 {
+		sum += n % 10
+		fmt.Println("sum:", sum)
+		n = n / 10
+		fmt.Println("n:", n)
+	}
+	fmt.Println("各位数的和sum为:", sum)
+	//test()
 
 	s := []int{7, 2, 8, -9, 4, 0}
 	// [:] :在数字（n）前面则表示从前面开始取数 取n个 反之从后面开始取
